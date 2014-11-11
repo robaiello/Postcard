957993e54042f359c03248d36005e74a330c0513
robaiello <robaiello@gmail.com>
2014-11-11 07:35:24 -0500

    Added name text label

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
     
@@ -28,14 +29,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-        //add comment to test commit
-       messageLabel.hidden = false
-       messageLabel.text = enterMessageTextField.text
+        //set message text
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
-        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        //set name text
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        //change button text
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
     }
